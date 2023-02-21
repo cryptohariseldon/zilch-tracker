2b9aef7588edc40348b9e9da98b992192318dad9
Hari Seldon <cryptoseldon33@proton.me>
2023-02-22 04:50:02 +0530

    naming

--- custom4/zilch-ledger2/src/lib.rs
+++ custom4/zilch-ledger2/src/lib.rs
@@ -1,42 +1,62 @@
 use solana_program::{
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    msg,
-    account_info::AccountInfo,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
 pub mod instruction;
-use instruction::{MovieInstruction};
+use instruction::ZilchInstruction;
 
 entrypoint!(process_instruction);
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    instruction_data: &[u8],
 ) -> ProgramResult {
     // Unpack called
-    let instruction = MovieInstruction::unpack(instruction_data)?;
+    let instruction = ZilchInstruction::unpack(instruction_data)?;
     // Match against the data struct returned into `instruction` variable
     match instruction {
-        MovieInstruction::AddMovieReview { title, rating, description } => {
+        ZilchInstruction::AddMovieReview {
+            title,
+            rating,
+            description,
+        } => {
             // Make a call to `add_move_review` function
-            add_movie_review(program_id, accounts, title, rating, description)
+            add_compute_request(program_id, accounts, title, rating, description)
         }
+        ZilchInstruction::AddProof {
+            proghash,
+            proof_account,
+            outputs,
+        } => add_proof(program_id, accounts, proghash, proof_account, outputs),
     }
 }
 
-pub fn add_movie_review(
+pub fn add_proof(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    rating: Pubkey,
+    description: String,
+) -> ProgramResult {
+    // Logging instruction data that was passed in
+    msg!("Adding proof review...");
+    msg!("Title: {}", title);
+    msg!("Rating: {}", rating);
+    msg!("Description: {}", description);
+
+    Ok(())
+}
+
+pub fn add_compute_request(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     title: String,
     rating: u8,
-    description: String
+    description: String,
 ) -> ProgramResult {
-
     // Logging instruction data that was passed in
-    msg!("Adding movie review...");
+    msg!("Adding comp rq...");
     msg!("Title: {}", title);
     msg!("Rating: {}", rating);
     msg!("Description: {}", description);
